8726ae2225610735ce827d05625fbf527812f61d
Pti2mike <mi_ca_elle@hotmail.com>
2024-03-05 16:04:03 +0100

    Export application into app.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
+const app = require("./app");
 const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const mongoose = require("mongoose");
-
-const mongoUrl = config.MONGODB_URI;
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogsRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
